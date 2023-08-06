c210b3c93675fccae65e96734305cf1d0a8c8f1e
Alexei <shchurko_alexei@mail.ru>
2023-08-06 18:32:59 +0300

    lesson11(задачи про коллекции и generic)

---
+++ lesson11/src/main/java/by/teachmeskills/c23onl/alexeishchurko/task11/interfaces/List.java
@@ -0,0 +1,15 @@
+package by.teachmeskills.c23onl.alexeishchurko.task11.interfaces;
+
+public interface List<T> {
+
+    //1
+    void add(T element);
+
+    //4
+    boolean contains(T element);
+
+    //5
+    void clear();
+
+    int size();
+}
